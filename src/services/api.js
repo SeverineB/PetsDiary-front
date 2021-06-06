2c2ca245651af1bcd8098bea411c0d8dc2c40ae2
SeverineBianchi <severine.bianchi78@gmail.com>
2021-06-07 01:59:05 +0200

    Fix mongo connection

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://pets-diary-server.severinebianchi.com/api/',
+  /* baseURL: 'http://pets-diary-server.severinebianchi.com/api/', */
+  baseURL: 'http://localhost:8000/api/',
   headers: {
     'Content-Type': 'application/json',
   },
