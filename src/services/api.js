4d893d7134c6e30dd4a6433d141d92540c9a8aaf
SeverineBianchi <severine.bianchi78@gmail.com>
2021-06-06 17:58:53 +0200

    Update services

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api/',
+  baseURL: 'http://pets-diary-server.severinebianchi.com/api/',
   headers: {
     'Content-Type': 'application/json',
   },
