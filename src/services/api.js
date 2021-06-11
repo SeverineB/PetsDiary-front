5f9ceefe8cbbfc8f4d35f9f86aac53c79cd1327e
SeverineBianchi <severine.bianchi78@gmail.com>
2021-06-11 08:41:28 +0200

    � Edit api variables

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:8000/api/' : 'http://pets-diary-server.severinebianchi.com/api/';
+
 const api = axios.create({
-  baseURL: 'http://pets-diary-server.severinebianchi.com/api/',
-  /* baseURL: 'http://localhost:8000/api/', */
+  baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
