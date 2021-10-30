41c7b8d8160aeccf0ef64e52e31349d6da8108d0
SeverineB <severine.bianchi78@gmail.com>
2021-10-30 13:45:26 +0200

    :lipstick: Edit font size

--- src/components/LoginPage/About/About.js
+++ src/components/LoginPage/About/About.js
@@ -6,11 +6,11 @@ const About = () => {
   return (
     <div className="about">
       <div className="about-infos">
-        <p className="about-infos-first">
+        <p className="about-infos-paragraph">
           Le <strong>carnet de santé</strong> en ligne de vos bêtes à poils ou à plumes.
         </p>
-        <p className="about-infos-second">
-          Retrouvez toutes les infos de votre animal préféré, prévoyez ses <strong>rendez-vous</strong>, suivez ses <strong>soins</strong>, tout ça sur une seule et même application ! 
+        <p className="about-infos-paragraph">
+          Retrouvez toutes les infos de votre animal préféré, prévoyez ses <strong>rendez-vous</strong>, suivez ses <strong>soins</strong>, tout ça sur une seule et même application&nbsp;! 
         </p>
       </div>
     </div>
