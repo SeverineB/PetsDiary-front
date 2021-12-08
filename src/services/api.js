ba1c20eec60fd3840d8cc0c9e8b0e877f98656d9
SeverineB <severine.bianchi78@gmail.com>
2021-12-09 00:22:45 +0100

    :bug: Try to fix blocked content bug

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api/' : 'https://pets-diary-server.severinebianchi.com/api/';
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api/' : 'http://pets-diary-server.severinebianchi.com/api/';
 const api = axios.create({
   baseURL,
   headers: {
